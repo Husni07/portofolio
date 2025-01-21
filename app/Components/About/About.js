90e58514d281370fcfa9193ecd3cf80e31613112
Husni07 <husnimubarrok07@gmail.com>
2025-01-21 13:27:03 +0700

    p

--- app/Components/About/About.js
+++ app/Components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <h1 className="m-auto font-light text-7xl text-black sm:text-7xl md:text-8xl xl:text-9xl">
         Coming soon
       </h1>
-      <div className="bg-white flex justify-between font-semibold text-base text-black">
+      <div className="bg-white flex flex-col sm:flex-row justify-between font-semibold text-base text-black">
         <h1>&copy; 2025 BARR <i>Foundation, Inc.</i> All rights reserved.</h1>
         <h1>Powered by BARR Office</h1>
       </div>

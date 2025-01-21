62252d441398406ea2ae39882c2cba09987210d1
Husni07 <husnimubarrok07@gmail.com>
2025-01-21 12:31:17 +0700

    p

--- app/Components/About/About.js
+++ app/Components/About/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='h-screen flex bg-white' id='about'>
-        <h1 className='m-auto font-light text-9xl text-black'>Coming soon</h1>
+    <div className='h-screen flex p-8 bg-white' id='about'>
+        <h1 className='m-auto font-light text-7xl text-black sm:text-7xl md:text-8xl xl:text-9xl'>Coming soon</h1>
     </div>
   )
 }

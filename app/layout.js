c3b503a357b807eda83d9836dd656156fa2b9510
Husni07 <husnimubarrok07@gmail.com>
2025-02-19 09:53:11 +0700

    p

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "BARR LABS By BARR FOUNDATION",
+  title: "BARR FOUNDATION",
   description: "LET'S WORK TOGETHER.",
 };
 
